2a882ca65e22e6c298372d1d61018fcbf34737f4
hush006 <alameenbusari70@gmail.com>
2025-12-15 10:39:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let mortAmount = document.getElementById('m-amount')
+let mortTerm = document.getElementById('m-term')
+let mortRate = document.getElementById('m-rate')
+
+// const calculation = mortAmountmortTerm, mortRate
+console.log(mortAmount, mortRate, mortTerm)
+
+
+function equation(x,y,z){
+    const equation = x / y * z
+    return equation;
+}
