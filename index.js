f64f9ad4c2c589b79cf1e79f8ff87fef0059c62c
hush006 <alameenbusari70@gmail.com>
2025-12-17 13:02:04 +0100

    new changes

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 let mortAmount = document.getElementById('m-amount')
 let mortTerm = document.getElementById('m-term')
 let mortRate = document.getElementById('m-rate')
+let form = document.querySelector('form')
 
 // const calculation = mortAmountmortTerm, mortRate
-console.log(mortAmount, mortRate, mortTerm)
+console.log(mortAmount, mortRate, mortTerm,form)
 
 
-function equation(x,y,z){
-    const equation = x / y * z
-    return equation;
-}
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    const inputedAmount = mortAmount.value;
+    const inputedRate = mortRate.value;
+    const inputedTerm = mortTerm.value;
+
+    const interestIncluded = (amount, rate , term)=>{
+        const myInterestEquation = amount/term * rate;
+        return myInterestEquation
+    }
+    const nonInterest = (amount, term)=>{
+        const myEquation = amount/term ;
+        return myEquation
+    }
+     console.log(nonInterest(inputedAmount, inputedTerm))
+
+     image.style.display = 'none'
+
+})
